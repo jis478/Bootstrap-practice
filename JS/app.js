9872f5d850fc386f2c884507295e5aec4b68b70c
jis478 <marksk82@gmail.com>
2023-05-28 21:14:52 +0900

    BULMA applied

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
 
-const scoreBoard = document.querySelector("#scoreBoard");
+// let scoreBoard = document.querySelector("#scoreBoard");
 const p1Display = document.querySelector("#p1Display")
 const p2Display = document.querySelector("#p2Display")
 const p1Button = document.querySelector("#p1Button");
@@ -9,16 +9,18 @@ const playingTo = document.querySelector("#playingTo");
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 8;
-let isGameOver = false;
+let winningScore = 0;
+let isGameOver = true;
 
 p1Button.addEventListener("click", function () {
     if (!isGameOver) {
         p1Score += 1;
         if (p1Score == winningScore) {
             isGameOver = true;
-            p1Display.classList.add("winner")
-            p1Display.classList.add("looser")
+            p1Display.classList.add("has-text-success");
+            p2Display.classList.add("has-text-danger");
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -29,8 +31,10 @@ p2Button.addEventListener("click", function () {
         p2Score += 1;
         if (p2Score == winningScore) {
             isGameOver = true;
-            p2Display.classList.add("winner")
-            p1Display.classList.add("looser")
+            p2Display.classList.add("has-text-success");
+            p1Display.classList.add("has-text-danger");
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -40,19 +44,19 @@ function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
-    scoreBoard.innerText = `${p1Score} to ${p2Score}`;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("has-text-success", "has-text-danger");
+    p2Display.classList.remove("has-text-success", "has-text-danger");
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
 
 resetButton.addEventListener("click", reset)
 
 playingTo.addEventListener("change", function () {
-    winningScore = this.value;
+    winningScore = parseInt(this.value);
+    console.log(winningScore)
     reset();
 })
 
-
-const login = async (username, password) => {
-    if (!username || !password) throw 'Missing Credentials'
-    if (password === "lalalala") return 'WELCOME'
-    throw 'Invalid Password'
-}
